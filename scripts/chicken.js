297cefa97bb3f99e2d9e66eb21ed474d37b292cd
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-15 10:52:27 -0500

    It is working

---
+++ scripts/chicken.js
@@ -0,0 +1,12 @@
+
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("chickenmonkey") // multiples of 5 and 7
+     } else if (currentNumber % 7 === 0) {
+        console.log("monkey") //multiples of 7
+     } else if (currentNumber % 5 === 0) {
+         console.log("chicken") //multiples of 5
+     } else {console.log(currentNumber)}
+}
