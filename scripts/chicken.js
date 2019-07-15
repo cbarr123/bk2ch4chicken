8a9fd578919b685301926297a0d8b526e4052375
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-15 10:53:41 -0500

    cleaned up and commented

--- scripts/chicken.js
+++ scripts/chicken.js
@@ -1,7 +1,4 @@
-
-
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-    
     if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
         console.log("chickenmonkey") // multiples of 5 and 7
      } else if (currentNumber % 7 === 0) {
